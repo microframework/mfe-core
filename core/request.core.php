184d5d83e31d1761cf7d5180a7a43a6c43596601
devinterx <devinterx@gmail.com>
2014-11-03 03:40:50 +0200

    Mix-tapes

---
+++ core/request.core.php
@@ -0,0 +1,37 @@
+<?php namespace mfe;
+/**
+ * Class Request
+ * @package mfe
+ */
+
+class RequestCore implements ImfeComponent {
+    const CORE_COMPONENT_NAME = 'RequestCore';
+    const CORE_COMPONENT_VERSION = '1.0.0';
+
+    public $is_ajax = false;
+
+    public function initRequest() {
+        global $_SERVER;
+    }
+
+    public function initProtocol_HTTP() {
+
+    }
+
+    public function initProtocol_HTTPS() {
+
+    }
+
+    public function initProtocol_CLI() {
+
+    }
+
+    public function initProtocol_AJAX() {
+
+    }
+
+    public function registerComponent() {
+    }
+}
+
+mfe::registerComponent('request', [get_called_class(), 'initRequest']);
---
+++ core/router.core.php
@@ -0,0 +1,27 @@
+<?php namespace mfe;
+/**
+ * Class RouterCore
+ * @package mfe
+ */
+
+mfe::dependence('request');
+
+class RouterCore implements ImfeComponent {
+    const CORE_COMPONENT_NAME = 'RouterCore';
+    const CORE_COMPONENT_VERSION = '1.0.0';
+
+    public $route = '';
+    protected $routePaths = [];
+
+    static public $mainRoute = '';
+
+    public function registerComponent() {
+        return true;
+    }
+
+    public function routerInit() {
+        $this->route = '';
+    }
+}
+
+mfe::registerCoreComponent('router', [get_called_class(), 'routerInit']);
